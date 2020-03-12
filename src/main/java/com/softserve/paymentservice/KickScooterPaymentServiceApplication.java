7ba7b9f219781ca813ab96e018af902fcc5cbf5f
Mary <60063717+yakovlieva811@users.noreply.github.com>
2020-03-12 19:55:52 +0200

    add comment

--- src/main/java/com/softserve/paymentservice/KickScooterPaymentServiceApplication.java
+++ src/main/java/com/softserve/paymentservice/KickScooterPaymentServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class KickScooterPaymentServiceApplication {
-
+//test
     public static void main(String[] args) {
         SpringApplication.run(KickScooterPaymentServiceApplication.class, args);
     }
