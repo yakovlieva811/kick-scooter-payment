c6b2f2f9b93ff27d9f905a88e798ab07a6ece467
Mary <60063717+yakovlieva811@users.noreply.github.com>
2020-03-12 18:05:27 +0200

    first one

---
+++ src/main/java/com/softserve/paymentservice/KickScooterPaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.softserve.paymentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KickScooterPaymentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KickScooterPaymentServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/softserve/paymentservice/KickScooterPaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softserve.paymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KickScooterPaymentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
